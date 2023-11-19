843da54d1ba78c03e3c9f36b052547e5a9d7c948
Tomasz Królikowski <t.krolikowski@gmail.com>
2023-11-19 22:34:29 +0100

    Lab 3

---
+++ Algorytmy_i_Struktury_Danych/zajęcia/Lab3.py
@@ -0,0 +1,21 @@
+# Zaimplementować wzór na obliczenie n-tego wyrazu ciągu Fibonacciego (wzór poniżej) w sposóbiteracyjny.
+# 0, n = 0
+# 1, n = 1
+# an−2 + an−1, n > 1
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(2, n+1):
+            c = a + b
+            a, b = b, c
+        return b
+
+
+n = int(input("Podaj liczbę n: "))
+result = fibonacci(n)
+print(f"Wartość {n}-tego wyrazu ciągu Fibonacciego: {result}")
